18249ea437f4afab75fe745c0479a40ce0b44b29
apple <apple@apples-MacBook-Pro.local>
2021-03-29 16:30:19 +0530

    SearchView added

--- app/src/main/java/com/codinginflow/imagesearchapp/gallery/ui/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/gallery/ui/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package com.codinginflow.imagesearchapp.gallery.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.codinginflow.imagesearchapp.R
@@ -34,10 +37,37 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+        setHasOptionsMenu(true)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+            inflater.inflate(R.menu.gallery_menu,menu)
+
+            val searchItem = menu.findItem(R.id.action_search)
+
+            val searchView = searchItem.actionView as SearchView
+
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                   if (query!=null){
+                       binding.recyclerView.scrollToPosition(0)
+                       viewModel.searchPhotos(query)
+                       searchView.clearFocus()
+                   }
+                    return true
+                }
+
+                override fun onQueryTextChange(p0: String?): Boolean {
+                   return true
+                }
+
+            })
     }
 }
